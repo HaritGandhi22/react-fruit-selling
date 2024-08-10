30f2e061630b53a829cda177b7bf0f1612a97d32
Harit Gandhi <harit.g@zillionica.com>
2024-08-10 16:01:22 +0530

    first commit

---
+++ my-project/src/components/Banners/Banner2.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import BannerPng from '../../assets/fruit-plate2.png'
+import { motion } from 'framer-motion'
+import { FadeUp, FadeLeft } from '../../utility/animation'
+
+const Banner2 = () => {
+    return (
+        <section className=''>
+            <div className="container grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14 md:py-24">
+                {/*Brand Info*/}
+                <div className='flex flex-col justify-center'>
+                    <div className='text-center md:text-left space-y-4 lg:max-w-[400px]'>
+                        <motion.h1
+                            variants={FadeUp(0.5)}
+                            initial='hidden'
+                            whileInView='visible'
+                            viewport={{ once: true }}
+                            className='text-3xl lg:text-6xl font-bold uppercase'>Online Fruit Store</motion.h1>
+                        <motion.p
+                            variants={FadeUp(0.7)}
+                            initial='hidden'
+                            whileInView='visible'
+                            viewport={{ once: true }}
+                        >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae iusto aliquid non sint alias sunt, consectetur voluptatibus reprehenderit, magnam corporis rerum minus doloremque veritatis ratione fugit. Rem facere quisquam pariatur!</motion.p>
+                        <motion.p
+                            variants={FadeUp(0.9)}
+                            initial='hidden'
+                            whileInView='visible'
+                            viewport={{ once: true }}
+                        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ex commodi et voluptatum officia quaerat.</motion.p>
+                        <motion.div
+                            variants={FadeUp(1.1)}
+                            animate='visible'
+                            initial='hidden'
+                            className='flex justify-center md:justify-start'>
+                            <button className='primary-btn'>
+                                Download App
+                            </button>
+                        </motion.div>
+                    </div>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <motion.img
+                        initial={{ opacity: 0, x: 200, rotate: 75 }}
+                        whileInView={{ opacity: 1, x: 0, rotate: 0 }}
+                        transition={{ delay: 0.2, duration: 1 }}
+                        viewport={{ once: true }}
+                        src={BannerPng} alt='' className='w-[350px] md:max-w-[500px] h-full object-cover drop-shadow' />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Banner2
